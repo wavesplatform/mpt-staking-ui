eb292f15729f7af847f9ddf76403cd3a14924a6d
Max Axenov <maksenov@wavesplatform.com>
2023-07-25 19:25:45 +0300

    add invoke stores

---
+++ src/stores/formStores.ts/unstakeStore.ts
@@ -0,0 +1,44 @@
+import { InvokeScriptCall, InvokeScriptPayment } from '@waves/ts-types';
+import {
+    BaseInputFormStore,
+    BaseInputFormStoreParams,
+} from '../utils/BaseInputFormStore';
+import { Money } from '@waves/data-entities';
+
+export class UnstakeStore extends BaseInputFormStore {
+    constructor(params: BaseInputFormStoreParams) {
+        super(params);
+    }
+
+    public get tx(): {
+        call: InvokeScriptCall<string | number> | null;
+        payment: Array<InvokeScriptPayment<string | number>> | null;
+    } {
+        return {
+            call: {
+                function: 'withdraw',
+                args: [{
+                    type: 'integer',
+                    value: this.currentAmount.getCoins().toNumber(),
+                }],
+            },
+            payment: [],
+        };
+    }
+
+    public invoke = () => {
+        const inputResult = this.checkInput();
+        if (!inputResult) {
+            return;
+        }
+        this.sendTransaction(() =>
+            this.rs.providerStore.sendInvoke(this.tx)
+        ).then(() => {
+            this.reset();
+        });
+    };
+
+    public get maxAmount(): Money | undefined {
+        return this.rs.contractStore.userContractData.data.availableToWithdraw;
+    }
+}
