46b54792b08806041b2466a062ec36dc98d8dfd9
vgakh1 <vgakh94@gmail.com>
2024-09-03 16:59:11 +0300

    DEXW-54: added staked nodes list

--- src/stores/utils/fetchNodeList.ts
+++ src/stores/utils/fetchNodeList.ts
@@ -1,9 +1,10 @@
 export interface INode {
 	address: string;
+    name: string;
 }
 
 export function fetchNodes(url: string): Promise<Array<INode>> {
     return fetch(url)
         .then((res) => res.json())
-        .then((data) => data.map((nodeConfig) => ({ address: nodeConfig.address })));
+        .then((data) => data.map((nodeConfig) => ({ address: nodeConfig.address, name: nodeConfig?.name || 'unknown' })));
 }
